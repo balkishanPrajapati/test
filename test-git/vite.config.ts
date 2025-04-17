251a27817132855315f96ec30ebc10e411261cea
balkishan <Balkishan.prajapati@webashlar.com>
2025-04-17 16:34:19 +0530

    removed work book inside the project

--- test-git/vite.config.ts
+++ test-git/vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-console.log(
-  "🧪 VITE_SECRET_KEY_NEW_TEST_KEY in vite.config.ts:",
-  process.env.VITE_SECRET_KEY_NEW_TEST_KEY
-);
+// console.log(
+//   "🧪 VITE_SECRET_KEY_NEW_TEST_KEY in vite.config.ts:",
+//   process.env.VITE_SECRET_KEY_NEW_TEST_KEY
+// );
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "import.meta.env.VITE_SECRET_KEY_NEW_TEST_KEY": JSON.stringify(
-      process.env.VITE_SECRET_KEY_NEW_TEST_KEY
-    ),
-  },
+  // define: {
+  //   "import.meta.env.VITE_SECRET_KEY_NEW_TEST_KEY": JSON.stringify(
+  //     process.env.VITE_SECRET_KEY_NEW_TEST_KEY
+  //   ),
+  // },
 });
